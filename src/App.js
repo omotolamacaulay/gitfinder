849899e0c76ef80932b8072b1ef5fa61187add3c
Omotola Macaulay <Tmac-thegreat@github.io>
2020-08-11 11:56:47 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
+import './App.css';
+
+class App extends Component {
+ render () {
+  return (
+    <div className="App">
+      <Navbar title = "Github Finder" icon ="fab fa-github"/>
+      <div className="container">
+        <Users />
+      </div>
+    </div>
+  );
+ }
+}
+
+export default App;
